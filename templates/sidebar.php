c422b3518781da1a3e7b3c3985acd71fc7796482
Daryl Lozupone <daryl@newclarity.net>
2016-09-27 23:03:06 +0000

    Add site list to sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -13,17 +13,17 @@
             <li><a href="database.php" data-title="Database"><i class="fa fa-link"></i> <span>Database</span></a></li>
             <li><a href="object-cache.php" data-title="Object Cache"><i class="fa fa-link"></i> <span>Object Caches</span></a></li>
             <li><a href="processvm.php" data-title="Process VM"><i class="fa fa-link"></i> <span>Process VM</span></a></li>
-<!--            <li class="treeview">-->
-<!--                <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>-->
-<!--                    <span class="pull-right-container">-->
-<!--                      <i class="fa fa-angle-left pull-right"></i>-->
-<!--                    </span>-->
-<!--                </a>-->
-<!--                <ul class="treeview-menu">-->
+            <li class="treeview">
+                <a href="#"><i class="fa fa-link"></i> <span>Sites</span>
+                    <span class="pull-right-container">
+                      <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
 <!--                    <li><a href="#">Link in level 2</a></li>-->
-<!--                    <li><a href="#">Link in level 2</a></li>-->
-<!--                </ul>-->
-<!--            </li>-->
+<?php $sites = json_decode(file_get_contents('/vagrant/pressboxx.json'), true)['sites']; array_walk(array_keys($sites), function($site_name){ printf('<li><a href="site.php" data-title="Site Administration" data-site="%1$s">%1$s</a></li>', $site_name);});?>
+                </ul>
+            </li>
         </ul>
         <!-- /.sidebar-menu -->
     </section>
