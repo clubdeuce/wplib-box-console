3e2e8a58402092fe0e59efe47523849b9c913f01
Daryl Lozupone <daryl@newclarity.net>
2016-09-28 16:08:35 +0000

    Fix API resource url

--- templates/site.php
+++ templates/site.php
@@ -4,7 +4,7 @@
 * The Site panel template
 */
 $site_id = filter_input(INPUT_GET, 'site_name', FILTER_SANITIZE_STRING);
-print_r($_GET);?>
+
 <div class="row">
 	<div class="col-md-12 col-sm-12 col-xs-12">
 		<div class="info-box">
@@ -14,8 +14,8 @@ print_r($_GET);?>
 <p><?php echo $site_id; ?></p>
 			<div class="info-box-content">
 				<div class="btn-group">
-					<span><button type="button" class="btn btn-success" data-url="/v1/box/site-backup/<?php echo $site_id;?>" data-method="post">Backup Site</button></span>
-					<span><button type="button" class="btn btn-warning" data-url="/v1/box/site-delete/<?php echo $site_id; ?>" data-method="post">Delete Site</button></span>
+					<span><button type="button" class="btn btn-success" data-url="/v1/site/<?php echo $site_id;?>/backup" data-method="post">Backup Site</button></span>
+					<span><button type="button" class="btn btn-warning" data-url="/v1/site/<?php echo $site_id; ?>/delete" data-method="post">Delete Site</button></span>
 				</div>
 			</div>
 		</div>
